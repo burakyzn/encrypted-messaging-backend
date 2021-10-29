d6ed6e79d60c23066ef11c90c1a9e85ba8a249ec
burakyzn <brkyznn@gmail.com>
2021-10-29 20:56:57 +0300

    first commit

---
+++ SecuredChatApp.WebApi/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecuredChatApp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new
+            {
+                resut = true,
+                data = "test answer"
+            });
+        }
+    }
+}

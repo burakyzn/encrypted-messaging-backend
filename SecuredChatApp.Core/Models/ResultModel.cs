4ab36427d6f3cc703d1e6ea6ae5fb0c249c24b65
burakyzn <brkyznn@gmail.com>
2021-10-30 22:50:07 +0300

    add result model

---
+++ SecuredChatApp.Core/Models/ResultModel.cs
@@ -0,0 +1,34 @@
+namespace SecuredChatApp.Core.Models
+{
+    public class ResultModel<T> where T : class
+    {
+        public ResultModel(T data = null, int statusCode = 200, ResultType type = ResultType.SUCCESS, string message = null)
+        {
+            this.statusCode = statusCode;
+            this.message = message;
+
+            if (type == ResultType.SUCCESS)
+            {
+                this.data = data;
+                this.success = true;
+            }
+            else
+            {
+                this.error = data;
+                this.success = false;
+            }
+        }
+
+        public int statusCode { get; set; }
+        public bool success { get; private set; }
+        public T data { get; set; }
+        public T error { get; set; }
+        public string message { get; set; }
+
+        public enum ResultType
+        {
+            SUCCESS,
+            FAIL
+        }
+    }
+}

aba81af9c09acd9705bbd998432e657a5ed030ff
Berkay Şahin <berkayshn97@gmail.com>
2021-11-06 22:49:34 +0300

    create add friend request

---
+++ SecuredChatApp.Core/DTOs/AddFriendRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SecuredChatApp.Core.DTOs
+{
+    public class AddFriendRequest
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string ToEmail { get; set; }
+    }
+}

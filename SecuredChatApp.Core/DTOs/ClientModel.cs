fd5f70603f9cdca849456b60d2fe9a759f198245
Berkay Şahin <berkayshn97@gmail.com>
2021-12-04 21:42:03 +0300

    add UserID property to ClientModel

--- SecuredChatApp.Core/DTOs/ClientModel.cs
+++ SecuredChatApp.Core/DTOs/ClientModel.cs
@@ -1,8 +1,11 @@
-﻿namespace SecuredChatApp.Core.DTOs
+﻿using System;
+
+namespace SecuredChatApp.Core.DTOs
 {
     public class ClientModel
     {
         public string ConnectionId { get; set; }
+        public Guid UserID { get; set; } 
         public string Nickname { get; set; }
     }
 }
